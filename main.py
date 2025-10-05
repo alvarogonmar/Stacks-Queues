b2c41b4aa4704f83029e91ac06f6199a3966a708
alvarogonmar <alvarogonmar0@gmail.com>
2025-10-05 13:08:07 -0600

    Add stack initialization and push operation to use_stack function

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ from stack import Stack
 def use_stack():
     print('=== Demo: Stack ===')
     s = Stack(10)
+    print(f'Se creó el stack con el elemento inicial: {s.peek()}')
+
+
+    # Agregar elementos
+    print('\n-- Operaciones push --')
+    s.push(20)
