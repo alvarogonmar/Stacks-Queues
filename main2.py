b5493d9d6a00559c30583099145fb982aa89a8b0
alvarogonmar <alvarogonmar0@gmail.com>
2025-10-17 10:03:55 -0600

    Add check for empty stack in main guard

--- main2.py
+++ main2.py
@@ -4,3 +4,6 @@ from stacksandqueues2 import Queue
 if __name__ == "__main__":
     print("=== Pruebas con la clase Stack ===")
     stack = Stack()
+
+    # Revisar si esta vacia al inicio
+    print("¿Está vacío?", stack.is_empty())
